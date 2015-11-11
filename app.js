6b3fee0579f7e2fa4597348381f312ee74fad102
traxlifuren <traxlifuren@gmail.com>
2015-11-11 21:34:34 +0100

    added html file foo.html

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log("Hello")
 
+var express = require('express');
+ 
+var server = express();
+server.use(express.static(__dirname + '/public'));
+ 
+var port = 10001;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
